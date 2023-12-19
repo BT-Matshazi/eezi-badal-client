20c86cce2aebf4cd8091356b30dd9783e2d458e8
BT-Matshazi <bekithembamatshazi@gmail.com>
2023-12-20 01:46:37 +0200

    box box

--- src/main.tsx
+++ src/main.tsx
@@ -2,35 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "/public/styles/globals.css";
+// import { BrowserRouter as Router } from "react-router-dom";
 
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-
-const router = createBrowserRouter([
-  {
-    path: "/vite-react-router/",
-    element: <App />,
-    children: [
-      {
-        path: "/vite-react-router/",
-        element: <Home />,
-      },
-      {
-        path: "/vite-react-router/login",
-        element: <Login />,
-      },
-      {
-        path: "/vite-react-router/register",
-        element: <Register />,
-      },
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <Router> */}
+      <App />
+    {/* </Router> */}
   </React.StrictMode>
 );
